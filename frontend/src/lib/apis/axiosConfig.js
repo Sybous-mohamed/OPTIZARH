ba48955c4c23d8e31d0e8795b7b19200310fba1c
Sybous-mohamed <mohamed.sybous@gmail.com>
2026-05-01 23:04:24 +0100

    remove .env

--- frontend/src/lib/apis/axiosConfig.js
+++ frontend/src/lib/apis/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://optizarh-production.up.railway.app",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
